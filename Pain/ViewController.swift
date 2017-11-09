eb8ffd51bedf17985e1634f797ed1044c6e2962a
Dylan Agiman <kaylab@air-25.local>
2017-11-08 17:29:25 -0800

    initial file dump

---
+++ Pain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pain
+//
+//  Created by Kay Lab on 11/8/17.
+//  Copyright © 2017 Kay Lab. All rights reserved.
+//
+//file defines a subclass of /UI viewer
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+//not needed for first lesson
+ //   override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+ //   }
+
+
+}
+
